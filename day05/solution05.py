70bd8847a6659561cbed9d86aeea92a853f6c007
Bryan Covell <bpc6@vt.edu>
2022-12-25 18:51:58 -0500

    made day05 a little more functional

--- day05/solution05.py
+++ day05/solution05.py
@@ -1,5 +1,6 @@
 import functools
 from typing import List, Tuple
+from itertools import zip_longest
 
 import utils
 from stack import Stack
@@ -9,23 +10,25 @@ def move_blocks_1_at_time(
     num_to_move: int,
     from_col: int,
     to_col: int,
-    curr_stacks: List[Tuple[str]]
-) -> List[Tuple[str]]:
+    curr_stacks: Tuple[Tuple[str]]
+) -> Tuple[Tuple[str]]:
     if num_to_move == 0:
         return curr_stacks
 
     popped, from_stack = Stack[str].pop(curr_stacks[from_col])
     to_stack = Stack[str].push(popped, curr_stacks[to_col])
 
-    new_stacks = []
-    for idx in range(len(curr_stacks)):
+    def place_stack(idx: int) -> Tuple[str]:
         if idx == from_col:
-            new_stacks.append(from_stack)
+            return from_stack
         elif idx == to_col:
-            new_stacks.append(to_stack)
+            return to_stack
         else:
-            new_stacks.append(curr_stacks[idx])
-    return move_blocks_1_at_time(num_to_move - 1, from_col, to_col, new_stacks)
+            return curr_stacks[idx]
+
+    return move_blocks_1_at_time(
+        num_to_move - 1, from_col, to_col, tuple(map(place_stack, range(len(curr_stacks))))
+    )
 
 
 def move_blocks_at_once(
@@ -39,14 +42,18 @@ def move_blocks_at_once(
     return curr_stacks
 
 
-def build_stacks(s: str, curr_stacks: List[List[str]]) -> List[List[str]]:
-    for idx, c in enumerate(s):
-        if c.isalpha():
-            stack_idx = int(idx / 4)
-            for _ in range(stack_idx - len(curr_stacks) + 1):
-                curr_stacks.append([])
-            curr_stacks[stack_idx].insert(0, c)
-    return curr_stacks
+def build_stacks(s: str, curr_stacks):
+    def condense(list_list_chars):
+        """Takes a list of lists of chars. Each list of char will have 0 or 1
+        alpha chars. Extract the alpha char to a tuple of length 0 or 1."""
+        def alpha_or_empty(ch: str) -> str:
+            return ch if ch.isalpha() else ''
+        return map(lambda strs: tuple(functools.reduce(lambda a, b: a + alpha_or_empty(b), strs, '')), list_list_chars)
+
+    next_push = utils.chain(
+        [functools.partial(utils.split_by_count, 4), condense]
+    )
+    return (n + c for n, c in zip_longest(next_push(s), curr_stacks, fillvalue=()))
 
 
 def first_nonspace(s: str) -> str:
@@ -75,7 +82,7 @@ def build_and_sort(stacks, input_lines, sorting_fcn):
 
 if __name__ == '__main__':
     filename = 'test.txt'
-    stacks = build_and_sort([[]], utils.input_gen(filename), move_blocks_1_at_time)
+    stacks = build_and_sort(tuple(), utils.input_gen(filename), move_blocks_1_at_time)
     print(functools.reduce(lambda a, b: a + Stack[str].pop(b)[0], stacks, ''))
 
     # puzzle 2
